361084eaa3694692f601402df0f2e0efb42abb51
Akuya Ekorot <akuya-ekorot@yahshio-corp.com>
2023-08-16 10:01:55 +0300

    - [x] Break routes to routes/ folder - [x] initiate supabase client in a utils folder - [x] set up express with cors and url extended to true - [x] use upsert, instead of insert, to add people to the waitlist
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   routes/waitlist.js
    	modified:   server.js
    	new file:   utils/db.js

--- waitlist/server.js
+++ waitlist/server.js
@@ -1,41 +1,21 @@
-// initialize express server
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+// Routes imports
+const waitlist = require("./routes/waitlist");
+
+// Express setup
 const app = express();
-const port = process.env.PORT || 3000;
-require ('dotenv').config();
-
-// initialize body-parser to parse incoming parameters requests to req.body
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes setup
+app.use("/waitlist", waitlist);
+
+// Server setup
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-}
-);
-
-// import supabase library, initialize supabase client
-const { createClient } = require('@supabase/supabase-js');
-const supabaseUrl = 'https://xldizurqdzdhckzisfxh.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-
-// waitlist form submission
-app.post('/waitlist', async (req, res) => {
-  try {
-    const { email, username } = req.body;
-    // check if email is already in waitlist
-    const { data, error } = await supabase
-      .from('waitlist')
-      .insert([
-        { email: email }
-      ]);
-    // if email is already in waitlist, return error
-    if (error) throw error;
-    res.status(200).json({ data });
-  }
-  catch (error) {
-    res.status(400).json({ error: error.message });
-  } 
+  console.log(`Server listening on port ${port}`);
 });
-
-
