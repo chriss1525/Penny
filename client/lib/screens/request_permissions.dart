b6f29f8fdf58fb4ac93bd9561eaa90ebd7d268f3
Akuya Ekorot <akuya-ekorot@yashio-corp.com>
2023-08-25 17:18:49 +0300

    request permissions page

--- client/lib/screens/request_permissions.dart
+++ client/lib/screens/request_permissions.dart
@@ -9,14 +9,41 @@ class RequestPermissionsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(
-        child: const Text('Request Permissions'),
-        onPressed: () async {
-          SmsService().checkPermission().then((value) {
-            Provider.of<PennyProvider>(context, listen: false)
-                .setPermission(value!);
-          });
-        },
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(
+              flex: 3,
+            ),
+            Text(
+              'Penny needs permission to read your messages.',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            const Text(
+              'This is so that Penny can read your M-PESA messages and help you keep track of your spending.',
+            ),
+            const Spacer(
+              flex: 1,
+            ),
+            ElevatedButton(
+              child: const Text('Request Permissions'),
+              onPressed: () async {
+                SmsService().checkPermission().then((value) {
+                  Provider.of<PennyProvider>(context, listen: false)
+                      .setPermission(value!);
+                });
+              },
+            ),
+            const Spacer(
+              flex: 3,
+            ),
+          ],
+        ),
       ),
     );
   }
