76e1e9266ffd20aac0b823e5515bf07c10e8b3c6
chriss1525 <okothchris15@gmail.com>
2023-08-28 18:10:51 +0300

    revers access token retrieval

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -2,34 +2,20 @@ const express = require("express");
 const router = express.Router();
 const supabase = require("../utils/db.js");
 
-router.get("/", async (req, res) => {
+router.get("/:userID", async (req, res) => {
   try {
-    const access = req.header("Authorization");
-
-    if (!access) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-
-    // authenticate the user
-    const { data: user, error } = await supabase.auth.api.getUserByCookie(access);
-
-    if (error) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-
-    const userID = user.id;
+    const userID = req.params.userID;
 
     // Get all transactions for a specific user
-    const { data, error: transaction } = await supabase
+    const { data, error } = await supabase
       .from("transactions")
       .select("amount, transaction_type, balance, time, sender, recipient, sender_phone, recipient_phone, transaction_id")
-      .eq("user_uid", userID)
+      .eq("id", userID)
       .order("date", { ascending: false });
 
-    if (transaction.error) throw transaction.error;
+    if (error) throw error;
     res.status(200).json({ data });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ error });
   }
 });
