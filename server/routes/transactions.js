4a3ec94a1f26e92b8de32c3113f715b0ac65b4e6
chriss1525 <okothchris15@gmail.com>
2023-08-28 22:17:38 +0300

    switch getUser method to fetch, successfully authorize and allow user to interact with transacations

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,21 +1,47 @@
 const express = require("express");
 const router = express.Router();
-const supabase = require("../utils/db.js");
+const { supabase } = require("../utils/db.js");
+const fetch = require("node-fetch");
+require('dotenv').config();
 
-router.get("/:userID", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const userID = req.params.userID;
+    const access = req.header("Authorization");
 
-    // Get all transactions for a specific user
-    const { data, error } = await supabase
+    if (!access) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    // Fetch the user's transactions from the database using the user's access token
+    const userData = await fetch(`${process.env.SUPABASE_URL}/auth/v1/user`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${access}`,
+        apikey: process.env.SUPABASE_KEY,
+      },
+    });
+
+    const user = await userData.json();
+    console.log(user);
+
+    if (!user.ok) {
+      return res.status(401).json({ error: "Unauthorized" });
+    } 
+
+    // Get all transactions for a specific user using the correct column name
+    const { data, error } = await supabase 
       .from("transactions")
       .select("amount, transaction_type, balance, time, sender, recipient, sender_phone, recipient_phone, transaction_id")
-      .eq("id", userID)
+      .eq("user_id", user.id) 
       .order("date", { ascending: false });
 
-    if (error) throw error;
+    if (error) {
+      throw error;
+    }
+
     res.status(200).json({ data });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error });
   }
 });
