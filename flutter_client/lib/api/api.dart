a0e44673647e4ea0c82e360a34e8c53f18ef234c
Akuya Ekorot <akuyaekorot@gmail.com>
2023-08-30 11:50:25 +0300

    delete flutter client

--- flutter_client/lib/api/api.dart
+++
@@ -1,9 +0,0 @@
-import 'package:penny/api/transactions/transactions_api.dart';
-
-class Api {
-  final String baseUrl = 'http://localhost:3000/';
-
-  transactions(String baseUrl) async {
-    return TransactionsApi(baseUrl: baseUrl);
-  }
-}
