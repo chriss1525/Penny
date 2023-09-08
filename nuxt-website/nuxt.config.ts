44587b26d672de212cb35ac6ed73dbcce923c814
chriss1525 <okothchris15@gmail.com>
2023-09-08 16:11:34 +0300

    change the port nuxt serves to

--- nuxt-website/nuxt.config.ts
+++ nuxt-website/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  server: {
+    port: 3003,
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
 })
