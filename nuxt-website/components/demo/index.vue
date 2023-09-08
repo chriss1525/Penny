0f1deef1beb97e6cbe0e1c4e6eab76144ed513c6
chriss1525 <okothchris15@gmail.com>
2023-09-08 16:39:05 +0300

    change route

--- nuxt-website/components/demo/index.vue
+++ nuxt-website/components/demo/index.vue
@@ -1,16 +1,1 @@
-<template>
-  <section id="demo">
-    <appetize-io-demo />
-  </section>
-</template>
-
-<script>
-import AppetizeIoDemo from '~/components/AppetizeIoDemo.vue';
-
-export default {
-  components: {
-    'appetize-io-demo': AppetizeIoDemo,
-  },
-};
-</script>
 
--- nuxt-website/nuxt.config.ts
+++ nuxt-website/nuxt.config.ts
@@ -1,11 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  server: {
-    port: 3003,
-  },
-  router: {
-    base: '/penny/website/',
-  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
 })
