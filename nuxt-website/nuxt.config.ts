5d1c5988aae005fab4167ecce863cd1bf81a67d9
chriss1525 <okothchris15@gmail.com>
2023-09-08 16:14:42 +0300

    change route

--- nuxt-website/nuxt.config.ts
+++ nuxt-website/nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   server: {
     port: 3003,
   },
+  router: {
+    base: '/penny/website/',
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
 })
