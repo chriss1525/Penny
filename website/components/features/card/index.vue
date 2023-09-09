7388665003194bb15fb823f687912e49f1666d8f
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-09 12:46:16 +0300

    style feature section

--- website/components/features/card/index.vue
+++ website/components/features/card/index.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="section-body w-full flex flex-col gap-4">
-    <div class="feature w-full flex flex-row gap-16">
-      <div class="feature-copy flex flex-col gap-2 w-full">
-        <h3 class="text-2xl font-semibold">{{ title }}</h3>
-        <p>{{ description }}</p>
+    <div class="feature w-full h-full flex flex-col gap-4 border shadow-lg rounded bg-white overflow-hidden">
+      <div class="h-48 p-4">
+        <img class="object-contain h-full w-full" :src="photo.src" :alt="photo.alt" />
       </div>
-      <div class="feature-media w-full">
-        <img :src="photo.src" :alt="photo.alt" />
+      <div class="feature-copy flex flex-col gap-2 w-full p-4">
+        <h3 class="text-xl font-semibold">{{ title }}</h3>
+        <p>{{ description }}</p>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 interface FeaturesCardProps {
   title: string;
   description: string;
