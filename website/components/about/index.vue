66c3747480bc2f4db1fb086db66247c952103b56
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-09 13:58:40 +0300

    refactor about card

--- website/components/about/index.vue
+++ website/components/about/index.vue
@@ -1,62 +1,63 @@
 <template>
 	<section id="about" class="flex justify-center w-full pt-16 text-slate-900">
-		<div class="w-full max-w-4xl flex flex-col items-center text-center gap-8">
+		<div
+			class="w-full max-w-4xl flex flex-col items-center text-center gap-8"
+		>
 			<header class="section-header">
 				<h2 class="text-xl">About Us</h2>
 			</header>
 			<div class="flex flex-col gap-16 items-center">
 				<div class="flex flex-col gap-4 max-w-xl">
 					<h3 class="text-4xl font-semibold">Inspiration</h3>
-					<p>We made Penny to help you answer the question, "Where did my money go?"</p>
-					<p>By automatically tracking your money, Penny helps you understand your financial habits and help you
-						achieve your goals.</p>
+					<p>
+						We made Penny to help you answer the question, "Where
+						did my money go?"
+					</p>
+					<p>
+						By automatically tracking your money, Penny helps you
+						understand your financial habits and help you achieve
+						your goals.
+					</p>
 				</div>
 				<div class="flex flex-col gap-4 max-w-xl">
 					<h3 class="text-4xl font-semibold">Connect with us</h3>
 					<div class="flex flex-row gap-4 md:gap-16 p-4">
-						<div class="flex flex-col items-center gap-4 border shadow-lg rounded md:p-8 p-4">
-							<img class="w-24 rounded-full" src="/akuya.jpg" alt="Akuya Ekorot. Software Engineer" />
-							<div class="">
-								<h4 class="font-semibold text-lg">Akuya Ekorot</h4>
-								<p class="text-slate-800">Software Engineer</p>
-							</div>
-							<div class="flex flex-row gap-4 items-center p-2 mt-2">
-								<a target="blank" href="https://github.com/akuya-ekorot">
-									<img class="w-4 h-4" src="/github-mark.svg" alt="akuya's github profile" />
-								</a>
-								<a target="blank" href="https://twitter.com/akuya_ekorot">
-									<img class="h-4" src="/x-logo.png" alt="akuya's twitter profile" />
-								</a>
-								<a target="blank" href="https://linkedin.com/in/akuya-ekorot">
-									<img class="h-4" src="/linkedin-logo.png" alt="akuya's linkedin profile" />
-								</a>
-							</div>
-						</div>
-						<div class="flex flex-col items-center gap-4 border shadow-lg rounded md:p-8 p-4">
-							<img class="w-24 rounded-full" src="/chriss.jpeg" alt="Christine Okoth. Software Engineer" />
-							<div class="">
-								<h4 class="font-semibold text-lg">Chriss Okoth</h4>
-								<p class="text-slate-800">Software Engineer</p>
-							</div>
-							<div class="flex flex-row gap-4 items-center p-2 mt-2">
-								<a target="blank" href="https://github.com/chriss1525">
-									<img class="w-4 h-4" src="/github-mark.svg" alt="chriss's github profile" />
-								</a>
-								<a target="blank" href="https://twitter.com/diahchrissy">
-									<img class="h-4" src="/x-logo.png" alt="chriss's twitter profile" />
-								</a>
-								<a target="blank" href="https://linkedin.com/in/christine-okoth-6a7789214">
-									<img class="h-4" src="/linkedin-logo.png" alt="chriss's linkedin profile" />
-								</a>
-							</div>
-						</div>
+						<AboutCard
+							name="Akuya Ekorot"
+							img="/akuya.jpg"
+							title="Software Engineer"
+							:socials="{
+								githubUrl: 'https://github.com/akuya-ekorot',
+								linkedinUrl:
+									'https://linkedin.com/in/akuya-ekorot',
+								twitterUrl: 'https://twitter.com/akuya_ekorot',
+							}"
+						/>
+						<AboutCard
+							name="Christine Okoth"
+							img="/chriss.jpeg"
+							title="Software Engineer"
+							:socials="{
+								githubUrl: 'https://github.com/chriss1525',
+								linkedinUrl:
+									'https://linkedin.com/in/christine-okoth-6a7789214',
+								twitterUrl: 'https://twitter.com/DiahChrissy',
+							}"
+						/>
 					</div>
 				</div>
 				<div class="flex flex-col gap-8 items-center pb-8">
 					<div>
-						<a class="flex flex-row items-center gap-4" target="blank"
-							href="https://github.com/chriss1525/Penny">
-							<img class="w-8 h-8" src="/github-mark.svg" alt="Penny's github repository" />
+						<a
+							class="flex flex-row items-center gap-4"
+							target="blank"
+							href="https://github.com/chriss1525/Penny"
+						>
+							<img
+								class="w-8 h-8"
+								src="/github-mark.svg"
+								alt="Penny's github repository"
+							/>
 							<p>View source code</p>
 						</a>
 					</div>
