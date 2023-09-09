2c4cba39489f5dc4cc019e1bfea93589397c2577
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-09 12:53:31 +0300

    style team cards

--- website/components/about/index.vue
+++ website/components/about/index.vue
@@ -13,41 +13,41 @@
 				</div>
 				<div class="flex flex-col gap-4 max-w-xl">
 					<h3 class="text-4xl font-semibold">Connect with us</h3>
-					<div class="flex flex-row gap-24 p-4">
-						<div class="flex flex-col items-center gap-4">
+					<div class="flex flex-col md:flex-row gap-8 md:gap-16 p-4">
+						<div class="flex flex-col items-center gap-4 border shadow-lg rounded p-8">
 							<img class="w-24 h-24 rounded-full" src="/akuya.jpg" alt="Akuya Ekorot. Software Engineer" />
 							<div class="">
-								<h4 class="font-semibold text-lg">Akuya</h4>
+								<h4 class="font-semibold text-lg">Akuya Ekorot</h4>
 								<p class="text-slate-800">Software Engineer</p>
 							</div>
-							<div class="flex flex-row gap-4 items-center">
+							<div class="flex flex-row gap-4 items-center p-2 mt-2">
 								<a target="blank" href="https://github.com/akuya-ekorot">
-									<img class="w-8 h-8" src="/github-mark.svg" alt="akuya's github profile" />
+									<img class="w-4 h-4" src="/github-mark.svg" alt="akuya's github profile" />
 								</a>
 								<a target="blank" href="https://twitter.com/akuya_ekorot">
-									<img class="h-6" src="/x-logo.png" alt="akuya's twitter profile" />
+									<img class="h-4" src="/x-logo.png" alt="akuya's twitter profile" />
 								</a>
 								<a target="blank" href="https://linkedin.com/in/akuya-ekorot">
-									<img class="h-8" src="/linkedin-logo.png" alt="akuya's linkedin profile" />
+									<img class="h-4" src="/linkedin-logo.png" alt="akuya's linkedin profile" />
 								</a>
 							</div>
 						</div>
-						<div class="flex flex-col items-center gap-4">
+						<div class="flex flex-col items-center gap-4 border shadow-lg rounded p-8">
 							<img class="w-24 h-24 rounded-full" src="/chriss.jpeg"
 								alt="Christine Okoth. Software Engineer" />
 							<div class="">
 								<h4 class="font-semibold text-lg">Chriss Okoth</h4>
 								<p class="text-slate-800">Software Engineer</p>
 							</div>
-							<div class="flex flex-row gap-4 items-center">
+							<div class="flex flex-row gap-4 items-center p-2 mt-2">
 								<a target="blank" href="https://github.com/chriss1525">
-									<img class="w-8 h-8" src="/github-mark.svg" alt="chriss's github profile" />
+									<img class="w-4 h-4" src="/github-mark.svg" alt="chriss's github profile" />
 								</a>
 								<a target="blank" href="https://twitter.com/diahchrissy">
-									<img class="h-6" src="/x-logo.png" alt="chriss's twitter profile" />
+									<img class="h-4" src="/x-logo.png" alt="chriss's twitter profile" />
 								</a>
 								<a target="blank" href="https://linkedin.com/in/christine-okoth-6a7789214">
-									<img class="h-8" src="/linkedin-logo.png" alt="chriss's linkedin profile" />
+									<img class="h-4" src="/linkedin-logo.png" alt="chriss's linkedin profile" />
 								</a>
 							</div>
 						</div>
